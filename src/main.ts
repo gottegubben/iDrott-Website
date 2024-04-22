2e2b8eadec3715e774abdc916cd3cd06c5873462
gottegubben <lukas.soderberg0404@gmail.com>
2024-04-22 22:04:00 +0200

    Router is configured!

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,41 @@
-//All the css assets.
-import './assets/color.css'
-import './assets/font.css'
-import './assets/main.css'
+//Import all the css assets.
+import './assets/color.css';
+import './assets/font.css';
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import { createMemoryHistory, createRouter } from 'vue-router';
 
-import App from './App.vue'
+import App from './App.vue';
 
-const app = createApp(App)
+//Create the app.
+const app = createApp(App);
 
-app.use(createPinia())
+//Create Pinia.
+const pinia = createPinia();
 
-app.mount('#app')
+app.use(pinia);
+
+//Configure the routes and the router itself.
+import ViewHome from './views/ViewHome.vue';
+import ViewBoard from './views/ViewBoard.vue';
+import ViewActivities from './views/ViewActivities.vue';
+import ViewHackeDutd from './views/ViewHackeDutd.vue';
+
+const routes = [
+    { path: '/', component: ViewHome },
+    { path: '/board', component: ViewBoard },
+    { path: '/activities', component: ViewActivities },
+    { path: '/hackeutd', component: ViewHackeDutd }
+];
+
+const router = createRouter({
+    history: createMemoryHistory(),
+    routes: routes
+});
+
+app.use(router);
+
+//Mount the app on a div with the id = #app.
+app.mount('#app');
