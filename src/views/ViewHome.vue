8a3501ba996e99dc2f02aaf492ff40ee8dbcd09e
gottegubben <lukas.soderberg0404@gmail.com>
2024-04-25 09:00:35 +0200

    Fixed the image!
    The solution included adding a "display: block" tag to the image css. The image is normally inline.

--- src/views/ViewHome.vue
+++ src/views/ViewHome.vue
@@ -1,12 +1,16 @@
 <template>
-    <img src="../assets/images/iDrott-homepage.jfif" alt="hej">
+    <img id="start" src="../assets/images/iDrott-homepage.jfif" alt="homepage-image">
 </template>
 
 <style scoped>
-    img {
-        position: relative;
+    img#start {
+        display: block;
+
         width: 100%;
-        height: calc(100dvh - (var(--header-height)));
+        height: calc(100dvh - var(--header-height)); /* The viewport height minus the header will be the desired size of the image. */
         object-fit: cover;
+
+        padding: 0;
+        margin: 0;
     }
 </style>
