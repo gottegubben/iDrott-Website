e9bb2aaf8976a61a6b7f6ffd743a6e376b837e54
Lukas Söderberg <lukas.soderberg0404@gmail.com>
2024-04-25 17:06:55 +0200

    Cleaned up the html and tried to add the logo.
    Cleaned up the html by putting everything that was created earlier into a new vue component. This is to not make it more cluttered than it already is. Then I also tried to add the logo but the "flex-grow: 1" won't really work even though the parent is set to relative. Very strange. Problem for another day!

---
+++ src/assets/icons/iDrottLogo.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+    const props = defineProps({
+        //The width of the logo. Ex: width = "50px".
+        height: {
+            type: String,
+            required: true
+        }
+    });
+</script>
+
+<template>
+    <img :style="{'height': height}" src="../images/iDrott23Logo.png" alt="Logo">
+</template>
---
+++ src/components/Home/FrontImage.vue
@@ -0,0 +1,85 @@
+<script setup lang="ts">
+    import iDrottLogo from '../../assets/icons/iDrottLogo.vue';
+</script>
+
+<template>
+    <div id="img-container">
+        <img id="start" src="../../assets/images/iDrott-homepage.jfif" alt="homepage-image">
+        <div id="img-shadow"></div>
+        <div id="image-content" class="content-container">
+            <div id="logo-container">
+                <!--<iDrottLogo height="100%"></iDrottLogo>-->
+            </div>
+            <div id="text-container">
+                <h2>Welcome to iDrott</h2>
+                <h3>iDrott is the Computer Technology Section's sports association that organizes sports activities at least once a week. The association is responsible for putting together teams for the various Chalmers Championships (CM) in sports such as floorball, football, ghost ball and not least kubb.</h3>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    div#img-container {
+        position: relative;
+
+        width: 100%;
+        height: calc(100dvh - var(--header-height)); /* The viewport height minus the header will be the desired height of the image container. */
+
+        /* There should be a max size where the image doesn't have to scale. At least the height because imagine a super high cropped image... */
+        max-height: none;
+        max-width: none;
+    }
+    img#start {
+        width: 100%;
+        height: 100%;
+
+        object-fit: cover;
+
+        display: block;
+    }
+    div#img-shadow {
+        display: flex;
+        justify-content: center;
+
+        position: absolute;
+        bottom: 0;
+
+        height: 50%;
+        width: 100% ;
+
+        background-image: linear-gradient(to top, var(--color30), hsla(0, 0%, 0%, 0));
+    }
+
+    div#image-content {
+        position: absolute;
+        bottom: 0;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: flex-end;
+
+        height: 100%;
+        width: 100%;
+    }
+    div#logo-container {
+        position: relative;
+
+        flex-grow: 1;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        margin-top: var(--space-xxl);
+        margin-bottom: var(--space-xxl);
+    }
+    div#text-container {
+        margin-bottom: var(--space-lg);
+    }
+    h2, h3 {
+        color: var(--color60);
+    }
+    h3 {
+        font-weight: var(--font-weight-s);
+    }
+</style>
