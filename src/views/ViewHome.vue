127f1943b8ef348fdfb2f9202d10b0d6f7cb61f8
gottegubben <lukas.soderberg0404@gmail.com>
2024-04-25 09:03:47 +0200

    Added some css to the image!
    This is later going to dictate how large the image can be... or how big screen you actually can have before the image size doesn't continue to stretch and such.

--- src/views/ViewHome.vue
+++ src/views/ViewHome.vue
@@ -10,6 +10,10 @@
         height: calc(100dvh - var(--header-height)); /* The viewport height minus the header will be the desired size of the image. */
         object-fit: cover;
 
+        /* There should be a max size where the image doesn't have to scale. At least the height because imagine a super high cropped image... */
+        max-height: none;
+        max-width: none;
+
         padding: 0;
         margin: 0;
     }
