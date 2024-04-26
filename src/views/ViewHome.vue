e51388a6318afe514836962a7b90325c185ce805
gottegubben <lukas.soderberg0404@gmail.com>
2024-04-26 23:56:23 +0200

    Climbing svg + information text... but with bugs.
    Added the climbing svg and tried to make it work but have a lot of work to do. Then I also added the information text but the text doesn't want to show up above the svg... probably because the "z-index" property doesn't apply to "position: static" objects... but I'll have to look into it another day!

--- src/views/ViewHome.vue
+++ src/views/ViewHome.vue
@@ -1,15 +1,55 @@
 <script setup>
     import FrontImage from '@/components/Home/FrontImage.vue';
+
+    import Climbing from '@/assets/icons/Climbing.vue';
 </script>
 
 <template>
     <FrontImage></FrontImage>
 
+    <div id="info-container">
+        <Climbing></Climbing>
+        <div class="content-container">
+            <div id="grid">
+                <div></div>
+                <div id="text-container">
+                    <h2>We host practice sessions!</h2>
+                    <p>At least once a week, iDrott hosts a practice session for all students currently enrolled in the Computer Science program at Chalmers. The types of sessions themselves can range from a variety of different sports such as volleyball, basketball and table tennis.<br><br>To read more about upcoming events and activities, please refer yourself to the activities page!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <div id="contact-container">
-        <div class="content-container" style="height: inherit;"></div>
+        <div class="content-container" style="height: inherit;">
+            
+        </div>
     </div>
 </template>
 
 <style scoped>
+    svg {
+        position: absolute;
+    }
 
+    div#info-container {
+        display: flex;
+        justify-content: center;
+        width: 100%;
+        height: 700px;
+        object-fit: contain;
+    }
+
+    div.content-container, div#grid {
+        height: inherit;
+    }
+
+    div#grid {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+    }
+
+    p, h2 {
+        color: red;
+    }
 </style>
