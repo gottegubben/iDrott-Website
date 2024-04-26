be81b5254e8504f0123973b18851088f9a2ea9d9
Lukas Söderberg <lukas.soderberg0404@gmail.com>
2024-04-26 22:19:55 +0200

    Just tried some things...

--- src/views/ViewHome.vue
+++ src/views/ViewHome.vue
@@ -5,15 +5,11 @@
 <template>
     <FrontImage></FrontImage>
 
-    <div style="width: 100%; height: 800px;"></div>
-
     <div id="contact-container">
         <div class="content-container" style="height: inherit;"></div>
     </div>
 </template>
 
 <style scoped>
-    div#contact-container {
-        background-color: red;
-    }
+
 </style>
