1c3cc6aee377f451464b3d6f4b06189aab5e65e7
gottegubben <lukas.soderberg0404@gmail.com>
2024-04-27 12:52:47 +0200

    Added some dummy elements.
    Also removed the climbing.vue component just because it wasn't needed right now! Also added a bright color to the climbing to make sure I don't forget the color!

--- src/assets/icons/Climbing.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <img src="../images/test.svg" preserveAspectRatio="xMidYMid meet" alt="">
-</template>
-
-<style scoped>
-</style>
--- src/views/ViewHome.vue
+++ src/views/ViewHome.vue
@@ -12,6 +12,7 @@
                 <div id="text-container">
                     <h1>We host <span id="practice">practice</span> sessions!</h1>
                     <p>At least once a week, iDrott hosts a practice session for all students currently enrolled in the Computer Science program at Chalmers. The types of sessions themselves can range from a variety of different sports such as volleyball, basketball and table tennis.<br><br>To read more about upcoming events and activities, please refer yourself to the activities page!</p>
+                    <button style="width: 130px; height: 30px; background-color: var(--color10); border: 0; margin-bottom: var(--space-xl);"></button>
                 </div>
             </div>
         </div>
