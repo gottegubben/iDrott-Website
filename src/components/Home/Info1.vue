6d3b16f00a0c6aa06d4b69b69b74a49d6fe5d03c
gottegubben <lukas.soderberg0404@gmail.com>
2024-05-02 19:03:56 +0200

    Added the new iDrott logo!

---
+++ src/components/Home/Info1.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+    import Climbing from '@/assets/icons/Climbing.vue';
+</script>
+
+<template>
+    <div id="info-container">
+        <div class="content-container-1">
+            <div id="img">
+                <Climbing color="red"></Climbing>
+            </div>
+            <div id="grid">
+                <div></div>
+                <div id="text-container">
+                    <h1>We host <span id="practice">practice</span> sessions!</h1>
+                    <p>At least once a week, iDrott hosts a practice session for all students currently enrolled in the Computer Science program at Chalmers. The types of sessions themselves can range from a variety of different sports such as volleyball, basketball and table tennis.<br><br>To read more about upcoming events and activities, please refer yourself to the activities page!</p>
+                    <button style="width: 130px; height: 30px; background-color: var(--color10); border: 0; margin-bottom: var(--space-xl);"></button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    div#info-container {
+        position: relative;
+        display: flex;
+        justify-content: center;
+    }
+
+    div.content-container-1 {
+        object-position: top center;
+    }
+
+    div.content-container-1, div#grid {
+        height: inherit;
+    }
+
+    div#grid {
+        margin-top: var(--space-xxl);
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+    }
+
+    div#text-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+
+    p {
+        margin-top: 0;
+        font-weight: var(--font-weight-m);
+    }
+
+    span#practice {
+        color: var(--color10);
+    }
+</style>
--- src/views/ViewHome.vue
+++ src/views/ViewHome.vue
@@ -1,9 +1,9 @@
 <script setup>
     import FrontImage from '@/components/Home/FrontImage.vue';
-    import Info from '@/components/Home/Info.vue';
+    import Info1 from '@/components/Home/Info1.vue';
 </script>
 
 <template>
     <FrontImage></FrontImage>
-    <Info></Info>
+    <Info1></Info1>
 </template>
