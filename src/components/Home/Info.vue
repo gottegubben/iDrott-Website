aa62650605bf615e821ae045f8c53115d19d7208
gottegubben <lukas.soderberg0404@gmail.com>
2024-05-06 22:34:29 +0200

    Working on the button component!

--- src/components/Home/Info.vue
+++ src/components/Home/Info.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+    import Button from '../Button.vue';
+</script>
+
 <template>
     <div id="info-container">
         <div class="content-container-1">
@@ -6,7 +10,7 @@
                 <div id="text-container">
                     <h1>We host <span id="practice">practice</span> sessions!</h1>
                     <p>At least once a week, iDrott hosts a practice session for all students currently enrolled in the Computer Science program at Chalmers. The types of sessions themselves can range from a variety of different sports such as volleyball, basketball and table tennis.<br><br>To read more about upcoming events and activities, please refer yourself to the activities page!</p>
-                    <button style="width: 130px; height: 30px; background-color: var(--color10); border: 0; margin-bottom: var(--space-xl);"></button>
+                    <Button color="var(--color10)" colorHover="var(--color10-sh1)" text="Test" textSize="var(--font-size-m)"></Button>
                 </div>
             </div>
         </div>
@@ -45,6 +49,7 @@
 
     p {
         margin-top: 0;
+        margin-bottom: var(--space-lg);
         font-weight: var(--font-weight-m);
     }
 
