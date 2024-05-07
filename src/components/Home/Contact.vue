7b14ad546797609e229a2ada225a16274e57d6c6
gottegubben <lukas.soderberg0404@gmail.com>
2024-05-07 11:06:31 +0200

    Did some work on the "contact us" section!

---
+++ src/components/Home/Contact.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+    import Button from '../Button.vue';
+</script>
+
+<template>
+    <div id="contact-container">
+        <div class="content-container">
+            <div id="contact-us">
+                <h2>Any questions?</h2>
+                <div id="btn-container">
+                    <Button text="Contact us!" color="var(--color10)" colorHover="var(--color10-sh1)"></Button>
+                </div>
+            </div>
+            <div></div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    h2 {
+        color: var(--color60);
+    }
+
+    div#contact-container {
+        display: flex;
+        justify-content: center;
+
+        width: 100%;
+
+        background-color: var(--color30);
+    }
+
+    div.content-container {
+        height: inherit;
+
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+    }
+
+    div#contact-us {
+        display: flex;
+        flex-direction: column;
+    }
+</style>
--- src/views/ViewHome.vue
+++ src/views/ViewHome.vue
@@ -1,9 +1,11 @@
 <script setup>
     import FrontImage from '@/components/Home/FrontImage.vue';
     import Info from '@/components/Home/Info.vue';
+    import Contact from '@/components/Home/Contact.vue';
 </script>
 
 <template>
     <FrontImage></FrontImage>
     <Info></Info>
+    <Contact></Contact>
 </template>
