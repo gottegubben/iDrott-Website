75f641ed05d20191dc44d9f6caad8d8c42f76a36
Lukas Söderberg <lukas.soderberg0404@gmail.com>
2024-05-08 20:07:15 +0200

    Created some boilerplate for the card links!

--- src/components/Home/ReadMore.vue
+++ src/components/Home/ReadMore.vue
@@ -1,15 +1,33 @@
+<script setup lang="ts">
+    import CardLink from './CardLink.vue';
+
+    const cardLinks = [
+        {
+            text: "Meet the board!",
+            imgSrc: ""
+        }
+    ];
+</script>
+
 <template>
     <div id="read-more-container">
         <div class="content-container">
             <h2>More <span>sports</span> related topics</h2>
             <div id="card-container">
-                
+                <CardLink v-for="cards in cardLinks"></CardLink>
             </div>
         </div>
     </div>
 </template>
 
 <style scoped>
+    div.content-container {
+        margin-top: var(--space-lg);
+
+        display: flex;
+        justify-content: center;
+    }
+
     span {
         color: var(--color10);
     }
