4f74cb7249d5342fcb41b6e1c6abe3ffb97d5415
gottegubben <lukas.soderberg0404@gmail.com>
2024-09-21 17:19:57 +0200

    Changed the code to allow for use of Vue Router.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,23 @@
-import { createApp } from "vue";
+import { createApp } from "vue"; //For Vue init.
+
+import { createRouter, createMemoryHistory } from "vue-router"; //For Vue Router init.
+
+//Import all the views (pages).
+import HomeView from "./View/HomeView.vue";
+
+const routes = [
+    { path: '/', component: HomeView }
+];
+
+const router = createRouter({
+    history: createMemoryHistory(), //For playground purposes.
+    routes
+});
+
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+
+app.use(router); //Make sure the app uses the router.
+
+app.mount("#app"); //Mount the app on a div with id #app.
