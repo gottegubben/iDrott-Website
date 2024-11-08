66bd6aebaf9fde4c482044c96e3f25a4bf5a6c5d
Lukas Söderberg <lukas.soderberg0404@gmail.com>
2024-11-08 22:11:26 +0100

    Added tasks and also added some more configurations settings for when building the web app!

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,5 +3,8 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    outDir: "build"
+  }
 });
