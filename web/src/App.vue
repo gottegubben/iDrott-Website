b85d41c0c234f6a38c4c21a02f7b69a18d31ac69
Lukas Söderberg <lukas.soderberg0404@gmail.com>
2024-11-10 12:57:55 +0100

    Constructed a demo on how the page base will look like. I'm referring to the container that encapsulates all the content on the website.

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,3 +1,19 @@
+<!--
+  The App component will be the first component that is
+  injected to the page. It's only parent will be the body.
+  The App component will be the wrapper for all the content
+  that should be displayed on the actual website.
+-->
+
 <template>
-  <h1>Hello World!</h1>
+  <p>testing</p>
+  <div style="height: 50px; width: 50px;"><p>testing again</p></div>
+  <p v-for="i in 50">TESTING...</p>
 </template>
+
+<style>
+  /* @import url("./assets/css/variables/variables_import.css"); */
+
+  @import url("./assets/css/body.css");
+  @import url("./assets/css/app.css");
+</style>
