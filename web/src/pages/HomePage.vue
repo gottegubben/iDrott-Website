f129ff2ff489c1adfbcf6b6f7deb96402a439b55
Lukas Söderberg <Lukas.Soderberg@volvo.com>
2024-11-26 21:02:29 +0100

    Worked on implementing the about page... just doing some testing!

--- web/src/pages/HomePage.vue
+++ web/src/pages/HomePage.vue
@@ -1,6 +1,14 @@
 <template>
-    <p>This is home!</p>
+    <BaseLayout> 
+        <div id="people-container" style="display: grid; grid-template-columns: repeat(3, 1fr); width: 100%; height: 100%; gap: 30px; grid-auto-rows: 1fr;">
+            <div v-for="i in 6" style="background-color: black;">
+                <p>Dalxa</p>
+                <p>Ordförande</p>
+            </div>
+        </div>
+    </BaseLayout>
 </template>
 
 <script setup lang="ts">
+    import BaseLayout from '../assets/components/BaseLayout.vue';
 </script>
